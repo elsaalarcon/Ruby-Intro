5b78e20192f850e24092578c0205e2997a4b9346
elsaalarcon <elsa032017@gmail.com>
2021-02-12 19:32:33 -0800

    test sum works

---
+++ spec/simple_arithmetic_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../one/simple_arithmetic.rb'
+
+describe 'retun 10 if is true' do
+  it "the result of sum is 10" do
+    sum = 5 + 5
+    expect(sum).to eq 10
+  end
+end
+  
+
+# require_relative '../one/print_name.rb'
+
+# describe 'when print name should return Mark ' do
+#   it "print Mark" do
+#     name = 'Mark'   
+#     expect(name).to eq 'Mark'
+#   end
+# end
+ 
+ 
