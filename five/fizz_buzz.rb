c633972401f4f7514e21acae18e859d512048581
elsaalarcon <elsa032017@gmail.com>
2021-02-18 19:41:22 -0800

    fizzbuzz test pass

--- five/fizz_buzz.rb
+++ five/fizz_buzz.rb
@@ -1,9 +1,11 @@
 def fizz_buzz(number)
-  if number % 3 == 0 and number % 5 == 0 
+  if (number % 3).zero? && (number % 5).zero? 
     'fizzbuzz'
-  elsif number % 3 == 0 
+  elsif (number % 3).zero? 
     'fizz'
-  else number % 5 == 0 
+  elsif (number % 5).zero?
     'buzz'
+  else 
+    number
   end
 end
